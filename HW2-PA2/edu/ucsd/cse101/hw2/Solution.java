961114d22b4acab1118b0c308b3ad474645829ed
remixj <jianghongxiang@bupt.edu.cn>
2019-10-07 14:52:59 -0700

    second solution using hashset and hashmap

--- HW2-PA2/edu/ucsd/cse101/hw2/Solution.java
+++ HW2-PA2/edu/ucsd/cse101/hw2/Solution.java
@@ -24,6 +24,7 @@ public class Solution {
     int R, 
     int C
   ){
+    HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
     HashSet<Integer> reached = new HashSet<>();
     Queue<Integer> queue = new LinkedList<>();
     boolean has_start = false;
@@ -36,12 +37,17 @@ public class Solution {
       if (m.cols == C){
         has_end = true;
       }
+      if (!map.containsKey(m.rows)){
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(m.cols);
+        map.put(m.rows,array);
+      }else{
+        map.get(m.rows).add(m.cols);
+      }
     }
-
-    if (has_end == false || has_start == false){
+    if (has_start == false || has_end == false){
       return false;
     }
-
     while (!queue.isEmpty()){
       int cur = queue.poll();
       if (reached.contains(cur)){
@@ -51,13 +57,53 @@ public class Solution {
       if (cur == C){
         return true;
       }
-      for (MatrixDimensions x : dimensions){
-        if (x.rows != cur || reached.contains(x.cols)){
+      if(!map.containsKey(cur)){
+        continue;
+      }
+      for (int next : map.get(cur)){
+        if (reached.contains(next)){
           continue;
         }
-        queue.offer(x.cols);
+        queue.offer(next);
       }
     }
     return false;
+
+    // Using hash set but O(N^2)
+    // HashSet<Integer> reached = new HashSet<>();
+    // Queue<Integer> queue = new LinkedList<>();
+    // boolean has_start = false;
+    // boolean has_end = false;
+    // for (MatrixDimensions m : dimensions){
+    //   if (m.rows == R){
+    //     has_start = true;
+    //     queue.offer(m.cols);
+    //   }
+    //   if (m.cols == C){
+    //     has_end = true;
+    //   }
+    // }
+
+    // if (has_end == false || has_start == false){
+    //   return false;
+    // }
+
+    // while (!queue.isEmpty()){
+    //   int cur = queue.poll();
+    //   if (reached.contains(cur)){
+    //     continue;
+    //   }
+    //   reached.add(cur);
+    //   if (cur == C){
+    //     return true;
+    //   }
+    //   for (MatrixDimensions x : dimensions){
+    //     if (x.rows != cur || reached.contains(x.cols)){
+    //       continue;
+    //     }
+    //     queue.offer(x.cols);
+    //   }
+    // }
+    // return false;
   }
 }
